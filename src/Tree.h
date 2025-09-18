4b24b0a343e453a9a5f990e92b2cf033f35152d0
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-09-18 18:49:13 -0400

    fixed I/O and added tree!

---
+++ src/Tree.h
@@ -0,0 +1,20 @@
+using namespace std;
+
+class TreeNode{
+    public:
+    int val; 
+    TreeNode *left; 
+    TreeNode *right;    
+    TreeNode(): val(0), left(nullptr), right(nullptr){}
+    TreeNode (int x) : val (x), left(nullptr), right(nullptr){}
+    TreeNode (int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+
+};
+
+class Tree{
+   public:
+    TreeNode* root; //a pointer to the root!
+
+    //i would like to put the insert, delete functions in here? 
+    
+};  
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <sstream>
+#include "Tree.h"
 
 using namespace std;
 
+
+TreeNode* insert(TreeNode* root, int key){
+	if (root == nullptr){
+		return new TreeNode(key);
+	}
+	if (key < root ->val) { //if root val is greater, move to the left
+		root -> left = insert(root->left, key);
+	}
+	else{
+		root -> right = insert(root->right, key);
+
+	}
+	return root;
+}
+
+
 int main(){
 	cout << "Hello AVL!\n";
+	cout << "How many commands will you be inputting? :" ;
+
+	string cmd; 
+
+	int cmdNum;
+	for (int i = 0; i < cmdNum ; i++){
+		cin >> cmd; 
+		if (cmd == "insert"){
+			string name;
+			long long ID; 
+			cin >> ws;
+			getline(cin, name, '"');
+			getline(cin, name, '"');
+			cin >> ID; 
+			//add to insert function 
+
+		} else if (cmd == "remove"){
+			//call remove function
+		} else if (cmd == "printInorder"){
+			//call printInorder function
+
+		} else if (cmd == "removeInorder"){
+			//call remove inorderfunction
+		} else{
+			cout<< "Invalid Input"<< endl; 
+		}
+		
+		
+	}
+
+
 	return 0;
 }
